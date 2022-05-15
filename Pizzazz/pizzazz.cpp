81c7f92bad6d58a1a7cafd4b4523294b433e4860
wheelercj <chrisw289@gmail.com>
2022-05-15 13:13:46 -0700

    Move function comments to the header file

--- Pizzazz/pizzazz.cpp
+++ Pizzazz/pizzazz.cpp
@@ -4,7 +4,6 @@
 
 namespace pizzazz {
 
-    /* Hides or shows the terminal's cursor. */
     void show_cursor(bool choice) {
         if (choice)
             std::cout << ESC "[?25h";
@@ -12,7 +11,6 @@ namespace pizzazz {
             std::cout << ESC "[?25l";
     }
 
-    /* Hides or shows the terminal cursor's blinking. */
     void show_cursor_blink(bool choice) {
         if (choice)
             std::cout << ESC "[?12h";
@@ -20,27 +18,14 @@ namespace pizzazz {
             std::cout << ESC "[?12l";
     }
 
-    /* Changes the terminal window's title.
-       Doesn't work with Windows Terminal.
-    */
     void set_window_title(std::string title) {
         std::cout << ESC "]0;" + title + ESC "[";
     }
 
-    /* Changes the terminal window's title.
-       The new title can include any emoji/Unicode characters.
-       Doesn't work with Windows Terminal.
-    */
     void set_window_title(std::wstring title) {
         wprint(LESC "]0;" + title + LESC "[");
     }
 
-    /*  Prints a string that can have emoji/Unicode characters.
-        When calling this function, put an L in front of the string.
-        For example, `wprint(L"Hi! ✨");` prints `Hi! ✨`.
-        Microsoft's C++ compiler doesn't support printing Unicode
-        characters with code points greater than U+FFFF.
-    */
     void wprint(std::wstring message) {
 #ifdef _WIN32
         fflush(stdout);
@@ -52,116 +37,85 @@ namespace pizzazz {
 #endif
     }
 
-    /* Prints a message with style such as as color, italics, underline, etc. */
     void print_styled(Style style, std::string message) {
         set_style(style);
         std::cout << message;
         reset_style();
     }
 
-    /* Prints a message with style such as as color, italics, underline, etc. */
     void print_styled(Style style, std::wstring message) {
         set_style(style);
         wprint(message);
         reset_style();
     }
 
-    /* Prints a message with styles such as as color, italics, underline, etc. */
     void print_styled(std::vector<Style> styles, std::string message) {
         set_style(styles);
         std::cout << message;
         reset_style();
     }
 
-    /* Prints a message with styles such as as color, italics, underline, etc. */
     void print_styled(std::vector<Style> styles, std::wstring message) {
         set_style(styles);
         wprint(message);
         reset_style();
     }
 
-    /* Sets future output to have a style such as as color, italics, underline, etc. */
     void set_style(Style style) {
         std::cout << ESC "[" << int(style) << "m";
     }
 
-    /* Sets future output to have styles such as as color, italics, underline, etc. */
     void set_style(std::vector<Style> styles) {
         for (Style style : styles)
             std::cout << ESC "[" << int(style) << "m";
     }
 
-    /* Sets future output to have styles such as as color, italics, underline, etc. */
     void set_style(std::string ansi_code_after_esc) {
         std::cout << ESC + ansi_code_after_esc;
     }
 
-    /* Sets future output to NOT have any special styles. */
     void reset_style() {
         std::cout << "\x1b[0m";
     }
 
-    /* Prints a message with a chosen color.
-       The red, green, and blue variables must be in the range [0, 255].
-    */
     void print_rgb(unsigned red, unsigned green, unsigned blue, std::string message) {
         std::cout << ESC "[38;2;" << red << ";" << green << ";" << blue << "m";
         std::cout << message;
         reset_style();
     }
 
-    /* Prints a message with a chosen color.
-       The message can have emoji/Unicode characters.
-       The red, green, and blue variables must be in the range [0, 255].
-    */
     void print_rgb(unsigned red, unsigned green, unsigned blue, std::wstring message) {
         std::cout << ESC "[38;2;" << red << ";" << green << ";" << blue << "m";
         wprint(message);
         reset_style();
     }
 
-    /* Prints a message with a chosen background color.
-       The red, green, and blue variables must be in the range [0, 255].
-    */
     void print_bg_rgb(unsigned red, unsigned green, unsigned blue, std::string message) {
         std::cout << ESC "[48;2;" << red << ";" << green << ";" << blue << "m";
         std::cout << message;
         reset_style();
     }
 
-    /* Prints a message with a chosen background color.
-       The message can have emoji/Unicode characters.
-       The red, green, and blue variables must be in the range [0, 255].
-    */
     void print_bg_rgb(unsigned red, unsigned green, unsigned blue, std::wstring message) {
         std::cout << ESC "[48;2;" << red << ";" << green << ";" << blue << "m";
         wprint(message);
         reset_style();
     }
 
-    /* Prints a message at chosen coordinates. */
     void print_at(unsigned x, unsigned y, std::string message) {
         set_cursor_coords(x, y);
         std::cout << message;
     }
 
-    /* Prints a message at chosen coordinates.
-       The message can have emoji/Unicode characters.
-    */
     void print_at(unsigned x, unsigned y, std::wstring message) {
         set_cursor_coords(x, y);
         wprint(message);
     }
 
-    /* Moves the terminal's cursor to chosen coordinates.
-       In this coordinate system, x increases to the right
-       and y increases downwards.
-    */
     void set_cursor_coords(unsigned x, unsigned y) {
         std::cout << ESC "[" << y << ";" << x << "H";
     }
 
-    /* Detects the current coordinates of the terminal's cursor. */
     Coord get_cursor_coords() {
         std::cout << ESC "[6n";  // request coordinates in the format \x1b[y;xR
         char input;
@@ -185,37 +139,30 @@ namespace pizzazz {
         return coord;
     }
 
-    /* Saves the terminal cursor's current location to be restored later. */
     void save_cursor_location() {
         std::cout << ESC "[s";
     }
 
-    /* Restores the terminal cursor to a previously saved location. */
     void restore_cursor_location() {
         std::cout << ESC "[u";
     }
 
-    /* Moves the terminal cursor up one line. */
     void move_cursor_up(unsigned lines) {
         std::cout << ESC "[" << lines << "A";
     }
 
-    /* Moves the terminal cursor down one line. */
     void move_cursor_down(unsigned lines) {
         std::cout << ESC "[" << lines << "B";
     }
 
-    /* Moves the terminal cursor to the right one column. */
     void move_cursor_right(unsigned columns) {
         std::cout << ESC "[" << columns << "C";
     }
 
-    /* Moves the terminal cursor to the left one column. */
     void move_cursor_left(unsigned columns) {
         std::cout << ESC "[" << columns << "D";
     }
 
-    /* Detects the terminal window's current size in columns and rows. */
     Coord get_window_size() {
 #ifdef _WIN32
         HANDLE hStdIn = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -233,7 +180,6 @@ namespace pizzazz {
 #endif
     }
 
-    /* Waits for and gets a character input without an Enter keypress. */
     char getch_() {
 #ifdef _WIN32
         return _getch();
@@ -249,9 +195,6 @@ namespace pizzazz {
 #endif
     }
 
-    /* Gets a character input without an Enter keypress only if there is input.
-       Returns 0 if there is no input.
-    */
     char getch_if_kbhit() {
 #ifdef _WIN32
         if (_kbhit())
@@ -277,7 +220,6 @@ namespace pizzazz {
         tcsetattr(0, TCSANOW, &term);
     }
 
-    /* Detects whether a key has been pressed. */
     bool kbhit_() {
         int bytes_waiting;
         ioctl(0, FIONREAD, &bytes_waiting);
@@ -285,59 +227,42 @@ namespace pizzazz {
     }
 #endif
 
-    /* Inserts text at the terminal cursor's current location. */
     void insert(std::string text) {
         std::cout << ESC "[" << text.size() << "@";
         std::cout << ESC "[" << text.size() << "D";
         std::cout << text;
     }
 
-    /* Inserts text at the terminal cursor's current location.
-       The text can have emoji/Unicode characters.
-    */
     void insert(std::wstring text) {
         std::cout << ESC "[" << text.size() << "@";
         std::cout << ESC "[" << text.size() << "D";
         wprint(text);
     }
 
-    /* Deletes a chosen number of characters at the terminal cursor's current location.
-       All characters to the right of what is deleted move to the left.
-    */
     void delete_chars(unsigned count) {
         std::cout << ESC "[" << count << "P";
     }
 
-    /* Inserts empty lines at the terminal cursor's current location. */
     void insert_lines(unsigned count) {
         std::cout << ESC "[" << count << "L";
     }
 
-    /* Deletes a chosen number of lines at the terminal cursor's current location.
-       All characters below what is deleted move up.
-    */
     void delete_lines(unsigned count) {
         std::cout << ESC "[" << count << "M";
     }
 
-    /* Deletes all characters in the terminal window.
-       Use of this function is not recommended without using an alternate screen buffer.
-    */
     void clear_screen() {
         std::cout << ESC "[2J";
     }
 
-    /* Switches the terminal app to a different "window" in the terminal. */
     void alternate_screen_buffer() {
         std::cout << ESC "[?1049h";
     }
 
-    /* Restores the terminal app's original "window". */
     void restore_screen_buffer() {
         std::cout << ESC "[?1049l";
     }
 
-    /* Pauses the program for a chosen amount of time. */
     void sleep_(unsigned milliseconds) {
 #ifdef _WIN32
         Sleep(milliseconds);
@@ -353,5 +278,4 @@ namespace pizzazz {
     void set_window_width_to_80() {
         std::cout << ESC "[?3l";
     }
-
 }
--- Pizzazz/pizzazz.h
+++ Pizzazz/pizzazz.h
@@ -115,50 +115,199 @@ namespace pizzazz {
         bright_bg_white,
     };
 
+    /* Hides or shows the terminal's cursor. */
     void show_cursor(bool = true);
+    
+    /* Hides or shows the terminal cursor's blinking. */
     void show_cursor_blink(bool = true);
+    
+    /* Changes the terminal window's title.
+       Doesn't work with Windows Terminal.
+    */
     void set_window_title(std::string title);
+
+    /* Changes the terminal window's title.
+       The new title can include any emoji/Unicode characters.
+       Doesn't work with Windows Terminal.
+    */
     void set_window_title(std::wstring title);
+
+    /*  Prints a string that can have emoji/Unicode characters.
+        When calling this function, put an L in front of the string.
+        For example, `wprint(L"Hi! ✨");` prints `Hi! ✨`.
+        Microsoft's C++ compiler doesn't support printing Unicode
+        characters with code points greater than U+FFFF.
+    */
     void wprint(std::wstring message);
+    
+    /* Prints a message with style such as as color, italics, underline, etc. */
     void print_styled(Style style, std::string message);
+    
+    /* Prints a message with style such as as color, italics, underline, etc. */
     void print_styled(Style style, std::wstring message);
+    
+    /* Prints a message with styles such as as color, italics, underline, etc. */
     void print_styled(std::vector<Style> styles, std::string message);
+    
+    /* Prints a message with styles such as as color, italics, underline, etc. */
     void print_styled(std::vector<Style> styles, std::wstring message);
+    
+    /* Sets future output to have a style such as as color, italics, underline, etc. */
     void set_style(Style style);
+    
+    /* Sets future output to have styles such as as color, italics, underline, etc. */
     void set_style(std::vector<Style> styles);
+    
+    /* Sets future output to have styles such as as color, italics, underline, etc. */
     void set_style(std::string ansi_code_after_esc);
+    
+    /* Sets future output to NOT have any special styles. */
     void reset_style();
+
+    /* Prints a message with a chosen color.
+       The red, green, and blue variables must be in the range [0, 255].
+    */
     void print_rgb(unsigned red, unsigned green, unsigned blue, std::string message);
+
+    /* Prints a message with a chosen color.
+       The message can have emoji/Unicode characters.
+       The red, green, and blue variables must be in the range [0, 255].
+    */
     void print_rgb(unsigned red, unsigned green, unsigned blue, std::wstring message);
+
+    /* Prints a message with a chosen background color.
+       The red, green, and blue variables must be in the range [0, 255].
+    */
     void print_bg_rgb(unsigned red, unsigned green, unsigned blue, std::string message);
+
+    /* Prints a message with a chosen background color.
+       The message can have emoji/Unicode characters.
+       The red, green, and blue variables must be in the range [0, 255].
+    */
     void print_bg_rgb(unsigned red, unsigned green, unsigned blue, std::wstring message);
+    
+    /* Prints a message at chosen coordinates. */
     void print_at(unsigned x, unsigned y, std::string message);
+
+    /* Prints a message at chosen coordinates.
+       The message can have emoji/Unicode characters.
+    */
     void print_at(unsigned x, unsigned y, std::wstring message);
+
+    /* Moves the terminal's cursor to chosen coordinates.
+       In this coordinate system, x increases to the right
+       and y increases downwards.
+    */
     void set_cursor_coords(unsigned x, unsigned y);
+    
+    /* Detects the current coordinates of the terminal's cursor. */
     Coord get_cursor_coords();
+    
+    /* Saves the terminal cursor's current location to be restored later. */
     void save_cursor_location();
+    
+    /* Restores the terminal cursor to a previously saved location. */
     void restore_cursor_location();
+    
+    /* Moves the terminal cursor up one line. */
     void move_cursor_up(unsigned = 1);
+    
+    /* Moves the terminal cursor down one line. */
     void move_cursor_down(unsigned = 1);
+    
+    /* Moves the terminal cursor to the right one column. */
     void move_cursor_right(unsigned = 1);
+    
+    /* Moves the terminal cursor to the left one column. */
     void move_cursor_left(unsigned = 1);
+    
+    /* Detects the terminal window's current size in columns and rows. */
     Coord get_window_size();
+
+    /* Waits for and gets a character input without requiring an Enter keypress.
+       Ignores modifier keys. Some keys return values that may be unexpected:
+       Enter -> '\\r',
+       Tab -> '\\t',
+       Escape -> '\\x1b',
+       Backspace -> '\\b',
+       Delete -> 'à' and then 'S',
+       up arrow -> 'à' and then 'H',
+       down arrow -> 'à' and then 'P',
+       left arrow -> 'à' and then 'K',
+       right arrow -> 'à' and then 'M',
+       Home -> 'à' and then 'G',
+       End -> 'à' and then 'O',
+       Page Up -> 'à' and then 'I',
+       Page Down -> 'à' and then 'Q',
+       Insert -> 'à' and then 'R',
+       F1 -> '\\0' and then ';',
+       F2 -> '\\0' and then '<',
+       F3 -> '\\0' and then '=',
+       F4 -> '\\0' and then '>',
+       F5 -> '\\0' and then '?',
+       F6 -> '\\0' and then the at symbol,
+       F7 -> '\\0' and then 'A',
+       F8 -> '\\0' and then 'B',
+       F9 -> '\\0' and then 'C',
+       F10 -> '\\0' and then 'D',
+       F11 -> 'à' and then '…',
+       F12 -> 'à' and then '†'.
+    */
     char getch_();
+
+    /* Gets a character input without an Enter keypress only if there is input.
+       Returns 0 if there is no input.
+    */
     char getch_if_kbhit();
+
 #ifndef _WIN32
+    
     void enable_raw_mode();
+    
     void disable_raw_mode();
+    
+    /* Detects whether a key has been pressed. */
     bool kbhit_();
 #endif
+    
+    /* Inserts text at the terminal cursor's current location. */
     void insert(std::string text);
+
+    /* Inserts text at the terminal cursor's current location.
+       The text can have emoji/Unicode characters.
+    */
     void insert(std::wstring text);
+
+    /* Deletes a chosen number of characters at the terminal cursor's current location.
+       All characters to the right of what is deleted move to the left.
+    */
     void delete_chars(unsigned count);
+    
+    /* Inserts empty lines at the terminal cursor's current location. */
     void insert_lines(unsigned count);
+
+    /* Deletes a chosen number of lines at the terminal cursor's current location.
+       All characters below what is deleted move up.
+    */
     void delete_lines(unsigned count);
+
+    /* Deletes all characters in the terminal window.
+       Use of this function is not recommended without using an alternate screen buffer.
+    */
     void clear_screen();
+    
+    /* Switches the terminal app to a different "window" in the terminal. */
     void alternate_screen_buffer();
+    
+    /* Restores the terminal app's original "window". */
     void restore_screen_buffer();
+
+    /* Pauses the program for a chosen amount of time.
+       1 second is 1000 milliseconds.
+    */
     void sleep_(unsigned milliseconds);
+    
     void set_window_width_to_132();
+    
     void set_window_width_to_80();
 }
