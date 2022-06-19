e9c8bad6d9a7fd50b1c24ae653538ee691b7733f
wheelercj <chrisw289@gmail.com>
2022-06-19 11:10:00 -0700

    Alphabetize includes and include Paginator.h

--- ynot/ynot.h
+++ ynot/ynot.h
@@ -1,7 +1,8 @@
 ﻿// version 1.5.0
 
 #pragma once
-#include "terminal.h"
-#include "InputField.h"
-#include "str.h"
 #include "common.h"
+#include "InputField.h"
+#include "Paginator.h"
+#include "str.h"
+#include "terminal.h"
